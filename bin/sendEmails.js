f725bbc8c6dfbd20c96a7544d5bba19c18b72ae7
Petr Kaspar <petr.kaspar@beesli.cz>
2022-03-30 22:47:37 +0200

    add correct JWT on prod  for sendEmails script

--- bin/sendEmails.js
+++ bin/sendEmails.js
@@ -7,7 +7,7 @@ axios({
         url: `${process.env.REACT_APP_GAME_BACKEND}/api/admin/cronSendEmails`,
         headers: { 
           'Content-Type': 'application/json',
-          'x-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImVtYWlsIjoiZGV2QWRtaW5AZW1haWwuY3oiLCJuYW1lIjoiYWRtaW5pc3RyYXRvciJ9fQ.4cK-HYQJHvTiA8u1ciGw9rIeodpCizAjQz1P3gQZI-I'
+          'x-token': process.env.REACT_APP_JWT_TOKEN
         },
       }).then((response) => {
     console.log(response.data);

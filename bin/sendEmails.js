a1814152c48337aa6d18f36e23836463b73e6399
Petr Kaspar <petr.kaspar@beesli.cz>
2022-03-30 22:35:04 +0200

    add backend prod url for sendEmails script

--- bin/sendEmails.js
+++ bin/sendEmails.js
@@ -4,7 +4,7 @@ console.log(new Date()); // Helps in checking the proper working
 if (new Date().getDay() === 3) {
 axios({
         method: 'get',
-        url: `${'http://localhost:3001'}/api/admin/cronSendEmails`,
+        url: `${process.env.REACT_APP_GAME_BACKEND}/api/admin/cronSendEmails`,
         headers: { 
           'Content-Type': 'application/json',
           'x-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImVtYWlsIjoiZGV2QWRtaW5AZW1haWwuY3oiLCJuYW1lIjoiYWRtaW5pc3RyYXRvciJ9fQ.4cK-HYQJHvTiA8u1ciGw9rIeodpCizAjQz1P3gQZI-I'

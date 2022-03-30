22a020858ced04f666037c0c5d4a166ba1e68061
Petr Kaspar <petr.kaspar@beesli.cz>
2022-03-30 22:17:38 +0200

    test(emails) Email notification sender set to pkaspar..

--- src/controllers/game/game.controller.js
+++ src/controllers/game/game.controller.js
@@ -464,7 +464,7 @@ export const sendEmailCronAdmin = async (req, res) => {
       });
 
     let htmlPage = newReservationEmailTemplate('Výpůjční doba je u konce!', 'Dovolujeme si Vás upozornit, že výpůjční doba her uvedených níže již dosáhla dvoutýdenní lhůty. Prosíme Vás tedy o jejich navrácení v následujících dnech během provozní doby půjčovny. Případně napište na udkh.vscht@gmail.com žádost o prodloužení výpůjční doby (žádosti nemusí být kvůli potřebám ÚDKH vyhověno).', borrowedGamesByEmail[0].userName, borrowedGamesByEmail[0].userEmail, '', gameItemsArray.join(''), `Doporučená výše dobrovolného daru za toto vypůjčení činí ${priceTotal} Kč. Děkujeme :-)`);
-      sendEmailMailjet('udkh.vscht@gmail.com', 'ÚDKH', 'pkaspar1@seznam.cz, `Upomínka`, htmlPage);
+      sendEmailMailjet('pkaspar1@seznam.cz', 'ÚDKH', 'pkaspar1@seznam.cz, `Upomínka`, htmlPage);
     });
 // borrowedGamesByEmail[0].userEmail
     return successResponse(req, res, uniqueEmails);

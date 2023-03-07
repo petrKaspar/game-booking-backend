7d638c5d239eb435f71d485f9e3b30994929a1a9
petrKaspar <petr1kaspar@gmail.com>
2023-03-07 20:37:42 +0100

    database: use DATABASE_URL from default postgesql heroku add-on

--- src/models/index.js
+++ src/models/index.js
@@ -14,12 +14,24 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config,
-  );
+//   sequelize = new Sequelize(
+//     config.database,
+//     config.username,
+//     config.password,
+//     config,
+//   );
+
+sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+}
+);
+
+
 }
 
 fs.readdirSync(__dirname)

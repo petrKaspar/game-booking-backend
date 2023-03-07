5676358a16cc9d0ce03ab89e2648947203522f7d
petrKaspar <petr1kaspar@gmail.com>
2023-03-07 20:42:27 +0100

    database: use DATABASE_URL for sequelize

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -2,25 +2,29 @@ const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require('./config')[env];
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME || config.database,
-  process.env.DB_USER || config.username,
-  process.env.DB_PASS || config.password,
-  {
-    host: '127.0.0.1',
-    port: 5433,
-    dialect: process.env.DB_DIALECT || config.dialect,
 
-  },
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME || config.database,
+//   process.env.DB_USER || config.username,
+//   process.env.DB_PASS || config.password,
+//   {
+//     host: '127.0.0.1',
+//     port: 5433,
+//     dialect: process.env.DB_DIALECT || config.dialect,
+
+//   },
+// );
+
+sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+}
 );
-/*
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-*/
+
 sequelize
   .authenticate()
   .then(() => {
